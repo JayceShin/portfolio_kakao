7b0a85fa8485223f278b276fe0c5d1b40d6a4585
JayceShin <31294995+JayceShin@users.noreply.github.com>
2021-10-11 02:55:49 +0900

    Update 2. selection_sort.py

--- 0. algorithm/1. sort/2. selection_sort.py
+++ 0. algorithm/1. sort/2. selection_sort.py
@@ -1,6 +1,6 @@
 class Selction:
-    def __init__(self):
-        pass
+    def __init__(self, array):
+        self.array = array
 
     # point "최소/최대", "선택"
     # 배열의 처음부터 끝까지 돌며 값을 비교하여 최소/최대값을 "선택"하고 교환
@@ -9,32 +9,32 @@ class Selction:
     # 최소/최대값을 "선택"하는 루프는 바깥쪽의 역순이기에 역시 n-1번 루프
     # 최소/최대값을 "선택"하기 위해 모든 경우를 따져야함 -> best, worst가 없이 동일
     # => O(N^2)
-    def sort(self, array, direction):
+    def sort(self, direction):
 
         if direction == "asc":
-            for i in range(0, len(array) - 1):
-                now = array[i]
+            for i in range(0, len(self.array) - 1):
+                now = self.array[i]
                 minValue = 99999
 
-                for j in range(i + 1, len(array)):
-                    if array[j] < minValue:
+                for j in range(i + 1, len(self.array)):
+                    if self.array[j] < minValue:
                         idx = j
-                        minValue = array[j]
+                        minValue = self.array[j]
 
                 if now > minValue:
-                    self.swap(array, i, idx)
+                    self.swap(self.array, i, idx)
         else:
-            for i in range(0, len(array) - 1):
-                now = array[i]
+            for i in range(0, len(self.array) - 1):
+                now = self.array[i]
                 maxValue = -99999
 
-                for j in range(i + 1, len(array)):
-                    if array[j] > maxValue:
+                for j in range(i + 1, len(self.array)):
+                    if self.array[j] > maxValue:
                         idx = j
-                        maxValue = array[j]
+                        maxValue = self.array[j]
 
                 if now < maxValue:
-                    self.swap(array, i, idx)
+                    self.swap(self.array, i, idx)
 
     # noinspection PyMethodMayBeStatic
     def swap(self, array, i, j):
@@ -45,6 +45,6 @@ class Selction:
 
 if __name__ == '__main__':
     a = [2, 1, 5, 4, 3]
-    selection = Selction()
-    selection.sort(a, "asc")
+    selection = Selction(a)
+    selection.sort("asc")
     print(a)
